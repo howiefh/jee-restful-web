7ba0abfe654722cdf77a5b1264211b34b78e5c7b
Feng Hao <howiefh@gmail.com>
2015-08-08 16:48:26 +0800

    删除原有前端代码，前端分离为独立项目 更新README.md

--- fe/src/js/sys/service/OrganizationService.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var sys = angular.module('sys');
-
-sys.factory('Organizations', ['Restangular', function(Restangular) {
-  var path = 'organizations';
-  var service = Restangular.service(path);  
-  return service;
-}]);
